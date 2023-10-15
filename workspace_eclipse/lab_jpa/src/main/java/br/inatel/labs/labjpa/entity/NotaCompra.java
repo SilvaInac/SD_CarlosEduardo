007c8a250df40709ffcab1edbb07c81242707e53
SilvaInac <eduardo040silva@gmail.com>
2023-10-14 22:43:51 -0300

    create getCalculoTotalnota

--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompra.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompra.java
@@ -1,5 +1,28 @@
 package br.inatel.labs.labjpa.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 public class NotaCompra {
+	
+	private Long id;
+	
+	private LocalDate dataEmissao;
+	
+	
+	private Fornecedor fornecedor;
+	
+	private List<NotaCompraItem> listaNotaCompraItem;
+	
+	//Calcula o total da nota somando os totais de cada item
+	
+	public BigDecimal getCalculoTotalNota() {
+		BigDecimal totalNota = listaNotaCompraItem.stream()
+			.map( i -> i.getCalculoTotalItem() )
+			.reduce( BigDecimal.ZERO,BigDecimal::add);
+		
+		return totalNota;
+	}
 
 }
