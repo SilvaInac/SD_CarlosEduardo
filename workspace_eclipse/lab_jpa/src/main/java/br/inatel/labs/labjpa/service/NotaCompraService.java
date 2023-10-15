5d1a1f07cea46333d150011cc0cf07d0db95b91b
SilvaInac <eduardo040silva@gmail.com>
2023-10-15 03:25:06 -0300

    add stringData

--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/NotaCompraService.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/NotaCompraService.java
@@ -1,12 +1,16 @@
 package br.inatel.labs.labjpa.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.inatel.labs.labjpa.entity.NotaCompra;
 import br.inatel.labs.labjpa.entity.NotaCompraItem;
+import br.inatel.labs.labjpa.repository.NotaCompraItemRepository;
+import br.inatel.labs.labjpa.repository.NotaCompraRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -14,40 +18,63 @@ import jakarta.persistence.PersistenceContext;
 @Transactional
 public class NotaCompraService {
 	
-	@PersistenceContext
-	private EntityManager em;
+	//@PersistenceContext
+	//private EntityManager em;
 	//notacompra
 	
+	@Autowired
+	private NotaCompraRepository notaCompraRepository;
+	
+	@Autowired
+	private NotaCompraItemRepository notaCompraItemRepository;
+	
 	public NotaCompra salvar(NotaCompra nc) {
-		return em.merge(nc);
+		//return em.merge(nc);
+		return notaCompraRepository.save(nc);
 	}
 	
-	public NotaCompra buscarNotaCompraPeloId(Long id) {
-		return em.find(NotaCompra.class, id);
+	public Optional<NotaCompra> buscarNotaCompraPeloId(Long id) {
+		//return em.find(NotaCompra.class, id);
+		return notaCompraRepository.findById(id);
 	}
 	
 	public NotaCompra buscarNotaCompraItemPeloIdComListaItem(Long id) {
-		NotaCompra notaCompra = em.find(NotaCompra.class,id);
-		notaCompra.getListaNotaCompraItem().size();
-		return notaCompra;
+		//NotaCompra notaCompra = em.find(NotaCompra.class,id);
+		//notaCompra.getListaNotaCompraItem().size();
+		//return notaCompra;
+		
+		Optional<NotaCompra> opNotaCompra = notaCompraRepository.findById(id);
+		if(opNotaCompra.isPresent()) {
+			NotaCompra notaCompra = opNotaCompra.get();
+			notaCompra.getListaNotaCompraItem().size();
+			return notaCompra;
+		}else {
+			throw new RuntimeException("Nenhuma nota encontrada");
+		}
+		
+		
 	}
 	
 	public List<NotaCompra> listNotaCompra(){
-		return em.createQuery("select nc from NotaCompra nc",NotaCompra.class)
-				.getResultList();
+		//return em.createQuery("select nc from NotaCompra nc",NotaCompra.class)
+		//		.getResultList();
+		return notaCompraRepository.findAll();
 	}
 	
 	//NotacompraItem
 	public NotaCompraItem salvar(NotaCompraItem item) {
-		return em.merge(item);
+		//return em.merge(item);
+		return notaCompraItemRepository.save(item);
 	}
 	
-	public NotaCompraItem buscarNotaCompraItemPeloId(Long id) {
-		return em.find(NotaCompraItem.class, id);
+	public Optional<NotaCompraItem> buscarNotaCompraItemPeloId(Long id) {
+		//return em.find(NotaCompraItem.class, id);
+		return notaCompraItemRepository.findById(id);
 	}
 		
 	public List<NotaCompraItem> listaNotaCompraItems(){
-		return em.createQuery("select i from NotaCompraItem i",NotaCompraItem.class)
-				.getResultList();
+		return notaCompraItemRepository.findAll();
+		//return em.createQuery("select i from NotaCompraItem i",NotaCompraItem.class)
+		//		.getResultList();
 	}
 }
--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/ProdutoService.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/ProdutoService.java
@@ -1,11 +1,14 @@
 package br.inatel.labs.labjpa.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.inatel.labs.labjpa.entity.Produto;
+import br.inatel.labs.labjpa.repository.ProdutoRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -13,28 +16,35 @@ import jakarta.persistence.PersistenceContext;
 @Transactional
 public class ProdutoService {
 	
-	@PersistenceContext
-	private EntityManager em;
+	//@PersistenceContext
+	//private EntityManager em;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public Produto salvart(Produto p) {
-		p=em.merge(p);
-		return p;
+		//p=em.merge(p);
+		//return p;
+		return produtoRepository.save(p);
 	}
 	
-	public Produto buscarPeloId(Long id) {
-		Produto produtoEncontrado = em.find(Produto.class, id);
-		return produtoEncontrado;
+	public Optional<Produto> buscarPeloId(Long id) {
+		return produtoRepository.findById(id);
+		//Produto produtoEncontrado = em.find(Produto.class, id);
+		//return produtoEncontrado;
 	}
 	
 	public List<Produto> listar(){
-		List<Produto> produtos = em.createQuery("select p from Produto p",Produto.class)
-			.getResultList();//JPQL
-		return produtos;
+		return produtoRepository.findAll();
+		//List<Produto> produtos = em.createQuery("select p from Produto p",Produto.class)
+		//	.getResultList();//JPQL
+		//return produtos;
 	}
 	
 	public void remove(Produto p) {
-		p = em.merge(p);
-		em.remove(p);
+		produtoRepository.delete(p);
+		//p = em.merge(p);
+		//em.remove(p);
 	}
 
 }
