c1c1bc3a074c321b772ad9c93de499a3ab7657ea
SilvaInac <eduardo040silva@gmail.com>
2023-10-15 03:32:14 -0300

    springData repository

--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/RelatorioService.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/RelatorioService.java
@@ -2,30 +2,35 @@ package br.inatel.labs.labjpa.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.inatel.labs.labjpa.dto.TotalCompradoPorFornecedorDTO;
+import br.inatel.labs.labjpa.repository.RelatorioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
 @Service
 public class RelatorioService {
 
-	@PersistenceContext
-	private EntityManager em;
+	//@PersistenceContext
+	//private EntityManager em;
+	
+	@Autowired
+	private RelatorioRepository repository;
 	
 	public List<TotalCompradoPorFornecedorDTO> pesquisarTotalCompradoPorFornecedor() {
-		String query = """
-				select br.inatel.labs.labjpa.dto.TotalCompradoPorFornecedorDTO
-					(f.razaoSocial
-					, sum(i.quantidade * i.valorCompraProduto)
-					)
-				from NotaCompraItem i
-					join i.notaCompra n
-					join n.fornecedor f
-				group by f.razaoSocial
-				""";
-		return em.createQuery(query,TotalCompradoPorFornecedorDTO.class).getResultList();
-		
+		//String query = """
+		//		select br.inatel.labs.labjpa.dto.TotalCompradoPorFornecedorDTO
+		//			(f.razaoSocial
+		//			, sum(i.quantidade * i.valorCompraProduto)
+		//			)
+		//		from NotaCompraItem i
+		//			join i.notaCompra n
+		//			join n.fornecedor f
+		//		group by f.razaoSocial
+		//		""";
+		//return em.createQuery(query,TotalCompradoPorFornecedorDTO.class).getResultList();
+		return repository.pesquisarTotalCompradoPorFornecedor();
 	}
 }
