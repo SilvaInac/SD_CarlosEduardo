8fea3755e2ecac4550361afda08a6f7e3387a92d
SilvaInac <eduardo040silva@gmail.com>
2023-10-14 22:37:11 -0300

    add class and sub package

---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Produto.java
@@ -0,0 +1,14 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.util.List;
+
+public class Produto {
+	
+	private Long idLong;
+	
+	private String descricai;
+	
+	private List<Fornecedor> listaFornecedor;
+	
+
+}
