f3e63cd3de3f0b9c7d8209971346b08aeeb19173
SilvaInac <eduardo040silva@gmail.com>
2023-10-14 22:56:44 -0300

    run testing

--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Produto.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Produto.java
@@ -2,13 +2,43 @@ package br.inatel.labs.labjpa.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Produto {
 	
-	private Long idLong;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
-	private String descricai;
+	@NotNull
+	@Size(min=2,max=100)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	
-	private List<Fornecedor> listaFornecedor;
+	//private List<Fornecedor> listaFornecedor;
+	
+	
 	
 
 }
