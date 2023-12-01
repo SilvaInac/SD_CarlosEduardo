121a9c25f1ff192c287ed35843e08b04511c5b1b
Carlos Eduardo da Silva Inácio <carlos.silva@inatel.br>
2023-12-01 18:15:20 -0300

    add lab rest client

---
+++ workspace_eclipse/lab_rest_server/src/main/java/controller/MyMessage.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class MyMessage {
+	private String info;
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+
+}
